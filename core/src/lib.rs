c4b9213ae6bf8ea9e9430edcb0532f544f1c2a52
Steve Stagg <stestagg@gmail.com>
2025-08-24 00:46:23 +0100

    Add basic radio transport module

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -97,3 +97,5 @@ impl ControlPacket {
         }
     }
 }
+
+pub mod radio;
--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -3,16 +3,22 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use esp_hal::peripherals::Peripherals;
 use panic_rtt_target as _;
-use rc_core::ControlPacket;
+use rc_core::{radio::Radio, ControlPacket};
 
 #[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
     rtt_target::rtt_init_defmt!();
     info!("receiver boot");
-    let _ = ControlPacket::FAILSAFE;
+
+    let mut peripherals = Peripherals::take();
+    let ieee = peripherals.IEEE802154;
+    let mut radio_clk = peripherals.RADIO_CLK;
+    let mut radio = Radio::new(ieee, &mut radio_clk);
+
     loop {
-        Timer::after(Duration::from_secs(1)).await;
+        let pkt = radio.receive().await;
+        info!("rx t={} s={} f={:?}", pkt.throttle, pkt.steering, pkt.flags);
     }
 }
